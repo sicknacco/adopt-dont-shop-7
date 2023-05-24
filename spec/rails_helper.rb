336b4cc2b3a5bfe36bcfa1630cc623a214ce51c4
Nick Sacco <nicksacco10@gmail.com>
2023-05-24 13:44:14 -0700

    refactor: changed all zips to strings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,24 +11,24 @@ def test_data
   @pet_5 = @shelter_2.pets.create!(adoptable: true, age: 4, breed: "tabby", name: "Pepper")
 
   @application_1 = Application.create!(name: "Bob", street_address: "1234 Southeast St",
-    city: "San Francisco", state: "CA", zip_code: 12345,
+    city: "San Francisco", state: "CA", zip_code: "AB345",
     description: "Wants a dog", status: "In Progress")
   @application_2 = Application.create!(name: "Sally", street_address: "4321 Bridge Way",
-    city: "San Francisco", state: "CA", zip_code: 54321,
+    city: "San Francisco", state: "CA", zip_code: "2234R",
     description: "Would like a siamese cat", status: "In Progress")
 
   @application_3 = Application.create!(name: "Fred", street_address: "376 Monroe St",
-    city: "Los Angeles", state: "CA", zip_code: 67890,
+    city: "Los Angeles", state: "CA", zip_code: "67890",
     description: "My wife really wants this cat", status: "In Progress")
   @application_4 = Application.create!(name: "Toki", street_address: "8395 Other St",
-    city: "Los Somewhere", state: "ZX", zip_code: 37163,
+    city: "Los Somewhere", state: "ZX", zip_code: "DA163",
     description: "We want this cat more than Fred", status: "In Progress")
   @application_5 = Application.create!(name: "Jimbo", street_address: "45865 Turnaround St",
-    city: "NYC", state: "NY", zip_code: 84930,
+    city: "NYC", state: "NY", zip_code: "PE930",
     description: "I want a big ol dog", status: "In Progress")
 
   @application_6 = Application.create!(name: "New Dude", street_address: "9375 Down Under St.",
-    city: "Upper Jabib", state: "PA", zip_code: 74625,
+    city: "Upper Jabib", state: "PA", zip_code: "74625",
     description: "Do you have any alligators?", status: "In Progress")
 
   PetApplication.create!(pet: @pet_1, application: @application_2)
